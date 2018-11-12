d7175ce080912af3fac619ec6c27180e05846ffa
FlyOrBoom <31298876+FlyOrBoom@users.noreply.github.com>
2018-11-12 10:14:57 -0800

    Add files via upload

--- set.js
+++ set.js
@@ -207,6 +207,16 @@ function score(varr) {
 }
 
 color();
+
+document.getElementById("reset").onclick = function reset() {
+    z = 0;
+    zero();
+    color();
+    document.getElementById("z").innerHTML = z + " points";
+}
+
+/*Red megagroup*/
+//1A//
 document.getElementById("a1Ad").onclick = function() {
     if (a1Ad === 0) {
         a++;
@@ -240,12 +250,14 @@ document.getElementById("a1Af").onclick = function() {
         score("a1Af");
     }
 }
+//1B//
 document.getElementById("a1Bd").onclick = function() {
     if (a1Bd === 0) {
         a++;
         n1++;
         B++;
         d++;
+        a1Bd++;
         x++;
         score("a1Bd");
     }
@@ -256,6 +268,7 @@ document.getElementById("a1Be").onclick = function() {
         n1++;
         B++;
         e++;
+        a1Be++;
         x++;
         score("a1Be");
     }
@@ -265,104 +278,861 @@ document.getElementById("a1Bf").onclick = function() {
         a++;
         n1++;
         B++;
-        f++;
+        f++
+        a1Bf++;
         x++;
         score("a1Bf");
     }
 }
-document.getElementById("a2C").onclick = function() {
-    if (a2C === 0) {
+//1C//
+document.getElementById("a1Cd").onclick = function() {
+    if (a1Cd === 0) {
+        a++;
+        n1++;
+        C++;
+        d++;
+        a1Cd++;
+        x++;
+        score("a1Cd");
+    }
+}
+document.getElementById("a1Ce").onclick = function() {
+    if (a1Ce === 0) {
+        a++;
+        n1++;
+        C++;
+        e++;
+        a1Ce++;
+        x++;
+        score("a1Ce");
+    }
+}
+document.getElementById("a1Cf").onclick = function() {
+    if (a1Cf === 0) {
+        a++;
+        n1++;
+        C++;
+        f++
+        a1Cf++;
+        x++;
+        score("a1Cf");
+    }
+}
+//2A//
+document.getElementById("a2Ad").onclick = function() {
+    if (a2Ad === 0) {
+        a++;
+        n2++;
+        A++;
+        d++;
+        a2Ad++;
+        x++;
+        score("a2Ad");
+    }
+}
+document.getElementById("a2Ae").onclick = function() {
+    if (a2Ae === 0) {
+        a++;
+        n2++;
+        A++;
+        e++;
+        a2Ae++;
+        x++;
+        score("a2Ae");
+    }
+}
+document.getElementById("a2Af").onclick = function() {
+    if (a2Af === 0) {
+        a++;
+        n2++;
+        A++;
+        f++
+        a2Af++;
+        x++;
+        score("a2Af");
+    }
+}
+//2B//
+document.getElementById("a2Bd").onclick = function() {
+    if (a2Bd === 0) {
+        a++;
+        n2++;
+        B++;
+        d++;
+        a2Bd++;
+        x++;
+        score("a2Bd");
+    }
+}
+document.getElementById("a2Be").onclick = function() {
+    if (a2Be === 0) {
+        a++;
+        n2++;
+        B++;
+        e++;
+        a2Be++;
+        x++;
+        score("a2Be");
+    }
+}
+document.getElementById("a2Bf").onclick = function() {
+    if (a2Bf === 0) {
+        a++;
+        n2++;
+        B++;
+        f++
+        a2Bf++;
+        x++;
+        score("a2Bf");
+    }
+}
+//2C//
+document.getElementById("a2Cd").onclick = function() {
+    if (a2Cd === 0) {
         a++;
         n2++;
         C++;
+        d++;
+        a2Cd++;
         x++;
-        score("a2C");
+        score("a2Cd");
     }
 }
-document.getElementById("a3A").onclick = function() {
-    if (a3A === 0) {
+document.getElementById("a2Ce").onclick = function() {
+    if (a2Ce === 0) {
+        a++;
+        n2++;
+        C++;
+        e++;
+        a2Ce++;
+        x++;
+        score("a2Ce");
+    }
+}
+document.getElementById("a2Cf").onclick = function() {
+    if (a2Cf === 0) {
+        a++;
+        n2++;
+        C++;
+        f++
+        a2Cf++;
+        x++;
+        score("a2Cf");
+    }
+}
+//3A//
+document.getElementById("a3Ad").onclick = function() {
+    if (a3Ad === 0) {
         a++;
         n3++;
         A++;
+        d++;
+        a3Ad++;
         x++;
-        score("a3A");
+        score("a3Ad");
     }
 }
-document.getElementById("b1B").onclick = function() {
-    if (b1B === 0) {
+document.getElementById("a3Ae").onclick = function() {
+    if (a3Ae === 0) {
+        a++;
+        n3++;
+        A++;
+        e++;
+        a3Ae++;
+        x++;
+        score("a3Ae");
+    }
+}
+document.getElementById("a3Af").onclick = function() {
+    if (a3Af === 0) {
+        a++;
+        n3++;
+        A++;
+        f++
+        a3Af++;
+        x++;
+        score("a3Af");
+    }
+}
+//3B//
+document.getElementById("a3Bd").onclick = function() {
+    if (a3Bd === 0) {
+        a++;
+        n3++;
+        B++;
+        d++;
+        a3Bd++;
+        x++;
+        score("a3Bd");
+    }
+}
+document.getElementById("a3Be").onclick = function() {
+    if (a3Be === 0) {
+        a++;
+        n3++;
+        B++;
+        e++;
+        a3Be++;
+        x++;
+        score("a3Be");
+    }
+}
+document.getElementById("a3Bf").onclick = function() {
+    if (a3Bf === 0) {
+        a++;
+        n3++;
+        B++;
+        f++
+        a3Bf++;
+        x++;
+        score("a3Bf");
+    }
+}
+//3C//
+document.getElementById("a3Cd").onclick = function() {
+    if (a3Cd === 0) {
+        a++;
+        n3++;
+        C++;
+        d++;
+        a3Cd++;
+        x++;
+        score("a3Cd");
+    }
+}
+document.getElementById("a3Ce").onclick = function() {
+    if (a3Ce === 0) {
+        a++;
+        n3++;
+        C++;
+        e++;
+        a3Ce++;
+        x++;
+        score("a3Ce");
+    }
+}
+document.getElementById("a3Cf").onclick = function() {
+    if (a3Cf === 0) {
+        a++;
+        n3++;
+        C++;
+        f++
+        a3Cf++;
+        x++;
+        score("a3Cf");
+    }
+}
+/*Green megagroup*/
+//1A//
+document.getElementById("b1Ad").onclick = function() {
+    if (b1Ad === 0) {
+        b++;
+        n1++;
+        A++;
+        d++;
+        b1Ad++;
+        x++;
+        score("b1Ad");
+    }
+}
+document.getElementById("b1Ae").onclick = function() {
+    if (b1Ae === 0) {
+        b++;
+        n1++;
+        A++;
+        e++;
+        b1Ae++;
+        x++;
+        score("b1Ae");
+    }
+}
+document.getElementById("b1Af").onclick = function() {
+    if (b1Af === 0) {
+        b++;
+        n1++;
+        A++;
+        f++
+        b1Af++;
+        x++;
+        score("b1Af");
+    }
+}
+//1B//
+document.getElementById("b1Bd").onclick = function() {
+    if (b1Bd === 0) {
         b++;
         n1++;
         B++;
+        d++;
+        b1Bd++;
         x++;
-        score("b1B");
+        score("b1Bd");
     }
 }
-document.getElementById("b2C").onclick = function() {
-    if (b2C === 0) {
+document.getElementById("b1Be").onclick = function() {
+    if (b1Be === 0) {
+        b++;
+        n1++;
+        B++;
+        e++;
+        b1Be++;
+        x++;
+        score("b1Be");
+    }
+}
+document.getElementById("b1Bf").onclick = function() {
+    if (b1Bf === 0) {
+        b++;
+        n1++;
+        B++;
+        f++
+        b1Bf++;
+        x++;
+        score("b1Bf");
+    }
+}
+//1C//
+document.getElementById("b1Cd").onclick = function() {
+    if (b1Cd === 0) {
+        b++;
+        n1++;
+        C++;
+        d++;
+        b1Cd++;
+        x++;
+        score("b1Cd");
+    }
+}
+document.getElementById("b1Ce").onclick = function() {
+    if (b1Ce === 0) {
+        b++;
+        n1++;
+        C++;
+        e++;
+        b1Ce++;
+        x++;
+        score("b1Ce");
+    }
+}
+document.getElementById("b1Cf").onclick = function() {
+    if (b1Cf === 0) {
+        b++;
+        n1++;
+        C++;
+        f++
+        b1Cf++;
+        x++;
+        score("b1Cf");
+    }
+}
+//2A//
+document.getElementById("b2Ad").onclick = function() {
+    if (b2Ad === 0) {
+        b++;
+        n2++;
+        A++;
+        d++;
+        b2Ad++;
+        x++;
+        score("b2Ad");
+    }
+}
+document.getElementById("b2Ae").onclick = function() {
+    if (b2Ae === 0) {
+        b++;
+        n2++;
+        A++;
+        e++;
+        b2Ae++;
+        x++;
+        score("b2Ae");
+    }
+}
+document.getElementById("b2Af").onclick = function() {
+    if (b2Af === 0) {
+        b++;
+        n2++;
+        A++;
+        f++
+        b2Af++;
+        x++;
+        score("b2Af");
+    }
+}
+//2B//
+document.getElementById("b2Bd").onclick = function() {
+    if (b2Bd === 0) {
+        b++;
+        n2++;
+        B++;
+        d++;
+        b2Bd++;
+        x++;
+        score("b2Bd");
+    }
+}
+document.getElementById("b2Be").onclick = function() {
+    if (b2Be === 0) {
+        b++;
+        n2++;
+        B++;
+        e++;
+        b2Be++;
+        x++;
+        score("b2Be");
+    }
+}
+document.getElementById("b2Bf").onclick = function() {
+    if (b2Bf === 0) {
+        b++;
+        n2++;
+        B++;
+        f++
+        b2Bf++;
+        x++;
+        score("b2Bf");
+    }
+}
+//2C//
+document.getElementById("b2Cd").onclick = function() {
+    if (b2Cd === 0) {
         b++;
         n2++;
         C++;
+        d++;
+        b2Cd++;
         x++;
-        score("b2C");
+        score("b2Cd");
     }
 }
-document.getElementById("b3A").onclick = function() {
-    if (b3A === 0) {
+document.getElementById("b2Ce").onclick = function() {
+    if (b2Ce === 0) {
+        b++;
+        n2++;
+        C++;
+        e++;
+        b2Ce++;
+        x++;
+        score("b2Ce");
+    }
+}
+document.getElementById("b2Cf").onclick = function() {
+    if (b2Cf === 0) {
+        b++;
+        n2++;
+        C++;
+        f++
+        b2Cf++;
+        x++;
+        score("b2Cf");
+    }
+}
+//3A//
+document.getElementById("b3Ad").onclick = function() {
+    if (b3Ad === 0) {
         b++;
         n3++;
         A++;
+        d++;
+        b3Ad++;
         x++;
-        score("b3A");
+        score("b3Ad");
     }
 }
-document.getElementById("b3B").onclick = function() {
-    if (b3B === 0) {
+document.getElementById("b3Ae").onclick = function() {
+    if (b3Ae === 0) {
+        b++;
+        n3++;
+        A++;
+        e++;
+        b3Ae++;
+        x++;
+        score("b3Ae");
+    }
+}
+document.getElementById("b3Af").onclick = function() {
+    if (b3Af === 0) {
+        b++;
+        n3++;
+        A++;
+        f++
+        b3Af++;
+        x++;
+        score("b3Af");
+    }
+}
+//3B//
+document.getElementById("b3Bd").onclick = function() {
+    if (b3Bd === 0) {
         b++;
         n3++;
         B++;
+        d++;
+        b3Bd++;
         x++;
-        score("b3B");
+        score("b3Bd");
     }
 }
-document.getElementById("c1C").onclick = function() {
-    if (a2C === 0) {
+document.getElementById("b3Be").onclick = function() {
+    if (b3Be === 0) {
+        b++;
+        n3++;
+        B++;
+        e++;
+        b3Be++;
+        x++;
+        score("b3Be");
+    }
+}
+document.getElementById("b3Bf").onclick = function() {
+    if (b3Bf === 0) {
+        b++;
+        n3++;
+        B++;
+        f++
+        b3Bf++;
+        x++;
+        score("b3Bf");
+    }
+}
+//3C//
+document.getElementById("b3Cd").onclick = function() {
+    if (b3Cd === 0) {
+        b++;
+        n3++;
+        C++;
+        d++;
+        b3Cd++;
+        x++;
+        score("b3Cd");
+    }
+}
+document.getElementById("b3Ce").onclick = function() {
+    if (b3Ce === 0) {
+        b++;
+        n3++;
+        C++;
+        e++;
+        b3Ce++;
+        x++;
+        score("b3Ce");
+    }
+}
+document.getElementById("b3Cf").onclick = function() {
+    if (b3Cf === 0) {
+        b++;
+        n3++;
+        C++;
+        f++
+        b3Cf++;
+        x++;
+        score("b3Cf");
+    }
+}
+/*Blue megagroup*/
+//1A//
+document.getElementById("c1Ad").onclick = function() {
+    if (c1Ad === 0) {
+        c++;
+        n1++;
+        A++;
+        d++;
+        c1Ad++;
+        x++;
+        score("c1Ad");
+    }
+}
+document.getElementById("c1Ae").onclick = function() {
+    if (c1Ae === 0) {
+        c++;
+        n1++;
+        A++;
+        e++;
+        c1Ae++;
+        x++;
+        score("c1Ae");
+    }
+}
+document.getElementById("c1Af").onclick = function() {
+    if (c1Af === 0) {
+        c++;
+        n1++;
+        A++;
+        f++
+        c1Af++;
+        x++;
+        score("c1Af");
+    }
+}
+//1B//
+document.getElementById("c1Bd").onclick = function() {
+    if (c1Bd === 0) {
+        c++;
+        n1++;
+        c++;
+        d++;
+        c1Bd++;
+        x++;
+        score("c1Bd");
+    }
+}
+document.getElementById("c1Be").onclick = function() {
+    if (c1Be === 0) {
+        c++;
+        n1++;
+        c++;
+        e++;
+        c1Be++;
+        x++;
+        score("c1Be");
+    }
+}
+document.getElementById("c1Bf").onclick = function() {
+    if (c1Bf === 0) {
+        c++;
+        n1++;
+        c++;
+        f++
+        c1Bf++;
+        x++;
+        score("c1Bf");
+    }
+}
+//1C//
+document.getElementById("c1Cd").onclick = function() {
+    if (c1Cd === 0) {
         c++;
         n1++;
         C++;
+        d++;
+        c1Cd++;
         x++;
-        score("c1C");
+        score("c1Cd");
     }
 }
-document.getElementById("c2A").onclick = function() {
-    if (c2A === 0) {
+document.getElementById("c1Ce").onclick = function() {
+    if (c1Ce === 0) {
+        c++;
+        n1++;
+        C++;
+        e++;
+        c1Ce++;
+        x++;
+        score("c1Ce");
+    }
+}
+document.getElementById("c1Cf").onclick = function() {
+    if (c1Cf === 0) {
+        c++;
+        n1++;
+        C++;
+        f++
+        c1Cf++;
+        x++;
+        score("c1Cf");
+    }
+}
+//2A//
+document.getElementById("c2Ad").onclick = function() {
+    if (c2Ad === 0) {
         c++;
         n2++;
         A++;
+        d++;
+        c2Ad++;
         x++;
-        score("c2A");
+        score("c2Ad");
     }
 }
-document.getElementById("c2B").onclick = function() {
-    if (c2B === 0) {
+document.getElementById("c2Ae").onclick = function() {
+    if (c2Ae === 0) {
         c++;
         n2++;
-        B++;
+        A++;
+        e++;
+        c2Ae++;
         x++;
-        score("c2B");
+        score("c2Ae");
     }
 }
-document.getElementById("c3C").onclick = function() {
-    if (c3C === 0) {
+document.getElementById("c2Af").onclick = function() {
+    if (c2Af === 0) {
+        c++;
+        n2++;
+        A++;
+        f++
+        c2Af++;
+        x++;
+        score("c2Af");
+    }
+}
+//2B//
+document.getElementById("c2Bd").onclick = function() {
+    if (c2Bd === 0) {
+        c++;
+        n2++;
+        c++;
+        d++;
+        c2Bd++;
+        x++;
+        score("c2Bd");
+    }
+}
+document.getElementById("c2Be").onclick = function() {
+    if (c2Be === 0) {
+        c++;
+        n2++;
+        c++;
+        e++;
+        c2Be++;
+        x++;
+        score("c2Be");
+    }
+}
+document.getElementById("c2Bf").onclick = function() {
+    if (c2Bf === 0) {
+        c++;
+        n2++;
+        c++;
+        f++
+        c2Bf++;
+        x++;
+        score("c2Bf");
+    }
+}
+//2C//
+document.getElementById("c2Cd").onclick = function() {
+    if (c2Cd === 0) {
+        c++;
+        n2++;
+        C++;
+        d++;
+        c2Cd++;
+        x++;
+        score("c2Cd");
+    }
+}
+document.getElementById("c2Ce").onclick = function() {
+    if (c2Ce === 0) {
+        c++;
+        n2++;
+        C++;
+        e++;
+        c2Ce++;
+        x++;
+        score("c2Ce");
+    }
+}
+document.getElementById("c2Cf").onclick = function() {
+    if (c2Cf === 0) {
+        c++;
+        n2++;
+        C++;
+        f++
+        c2Cf++;
+        x++;
+        score("c2Cf");
+    }
+}
+//3A//
+document.getElementById("c3Ad").onclick = function() {
+    if (c3Ad === 0) {
+        c++;
+        n3++;
+        A++;
+        d++;
+        c3Ad++;
+        x++;
+        score("c3Ad");
+    }
+}
+document.getElementById("c3Ae").onclick = function() {
+    if (c3Ae === 0) {
+        c++;
+        n3++;
+        A++;
+        e++;
+        c3Ae++;
+        x++;
+        score("c3Ae");
+    }
+}
+document.getElementById("c3Af").onclick = function() {
+    if (c3Af === 0) {
+        c++;
+        n3++;
+        A++;
+        f++
+        c3Af++;
+        x++;
+        score("c3Af");
+    }
+}
+//3B//
+document.getElementById("c3Bd").onclick = function() {
+    if (c3Bd === 0) {
+        c++;
+        n3++;
+        c++;
+        d++;
+        c3Bd++;
+        x++;
+        score("c3Bd");
+    }
+}
+document.getElementById("c3Be").onclick = function() {
+    if (c3Be === 0) {
+        c++;
+        n3++;
+        c++;
+        e++;
+        c3Be++;
+        x++;
+        score("c3Be");
+    }
+}
+document.getElementById("c3Bf").onclick = function() {
+    if (c3Bf === 0) {
+        c++;
+        n3++;
+        c++;
+        f++
+        c3Bf++;
+        x++;
+        score("c3Bf");
+    }
+}
+//3C//
+document.getElementById("c3Cd").onclick = function() {
+    if (c3Cd === 0) {
         c++;
         n3++;
         C++;
+        d++;
+        c3Cd++;
         x++;
-        score("c3C");
+        score("c3Cd");
     }
 }
-document.getElementById("reset").onclick = function reset() {
-    z = 0;
-    zero();
-    color();
-    document.getElementById("z").innerHTML = z + " points";
+document.getElementById("c3Ce").onclick = function() {
+    if (c3Ce === 0) {
+        c++;
+        n3++;
+        C++;
+        e++;
+        c3Ce++;
+        x++;
+        score("c3Ce");
+    }
+}
+document.getElementById("c3Cf").onclick = function() {
+    if (c3Cf === 0) {
+        c++;
+        n3++;
+        C++;
+        f++
+        c3Cf++;
+        x++;
+        score("c3Cf");
+    }
 }
