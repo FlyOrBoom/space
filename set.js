9e02f6030c0012cf908d60bf99c0b6b9cbc5b003
FlyOrBoom <31298876+FlyOrBoom@users.noreply.github.com>
2018-11-11 23:39:04 -0800

    Add files via upload

---
+++ set.js
@@ -0,0 +1,368 @@
+var a = 0;
+var b = 0;
+var c = 0;
+var n1 = 0;
+var n2 = 0;
+var n3 = 0;
+var A = 0;
+var B = 0;
+var C = 0;
+var d = 0;
+var e = 0;
+var f = 0;
+
+var x = 0;
+
+var z = 0;
+
+var a1Ad = 0; var a1Ae = 0; var a1Af = 0;
+var a1Bd = 0; var a1Be = 0; var a1Bf = 0;
+var a1Cd = 0; var a1Ce = 0; var a1Cf = 0;
+
+var a2Ad = 0; var a2Ae = 0; var a2Af = 0;
+var a2Bd = 0; var a2Be = 0; var a2Bf = 0;
+var a2Cd = 0; var a2Ce = 0; var a2Cf = 0;
+
+var a3Ad = 0; var a3Ae = 0; var a3Af = 0;
+var a3Bd = 0; var a3Be = 0; var a3Bf = 0;
+var a3Cd = 0; var a3Ce = 0; var a3Cf = 0;
+
+var b1Ad = 0; var b1Ae = 0; var b1Af = 0;
+var b1Bd = 0; var b1Be = 0; var b1Bf = 0;
+var b1Cd = 0; var b1Ce = 0; var b1Cf = 0;
+
+var b2Ad = 0; var b2Ae = 0; var b2Af = 0;
+var b2Bd = 0; var b2Be = 0; var b2Bf = 0;
+var b2Cd = 0; var b2Ce = 0; var b2Cf = 0;
+
+var b3Ad = 0; var b3Ae = 0; var b3Af = 0;
+var b3Bd = 0; var b3Be = 0; var b3Bf = 0;
+var b3Cd = 0; var b3Ce = 0; var b3Cf = 0;
+
+var c1Ad = 0; var c1Ae = 0; var c1Af = 0;
+var c1Bd = 0; var c1Be = 0; var c1Bf = 0;
+var c1Cd = 0; var c1Ce = 0; var c1Cf = 0;
+
+var c2Ad = 0; var c2Ae = 0; var c2Af = 0;
+var c2Bd = 0; var c2Be = 0; var c2Bf = 0;
+var c2Cd = 0; var c2Ce = 0; var c2Cf = 0;
+
+var c3Ad = 0; var c3Ae = 0; var c3Af = 0;
+var c3Bd = 0; var c3Be = 0; var c3Bf = 0;
+var c3Cd = 0; var c3Ce = 0; var c3Cf = 0;
+
+function size(idd) {
+    document.getElementById(idd).style.transform = "scale(1,1)";
+}
+
+function zero() {
+    a = 0;
+    b = 0;
+    c = 0;
+    n1 = 0;
+    n2 = 0;
+    n3 = 0;
+    A = 0;
+    B = 0;
+    C = 0;
+    d = 0;
+    e = 0;
+    f = 0;
+
+    x = 0;
+
+    a1Ad = 0; a1Ae = 0; a1Af = 0;
+    a1Bd = 0; a1Be = 0; a1Bf = 0;
+    a1Cd = 0; a1Ce = 0; a1Cf = 0;
+
+    a2Ad = 0; a2Ae = 0; a2Af = 0;
+    a2Bd = 0; a2Be = 0; a2Bf = 0;
+    a2Cd = 0; a2Ce = 0; a2Cf = 0;
+
+    a3Ad = 0; a3Ae = 0; a3Af = 0;
+    a3Bd = 0; a3Be = 0; a3Bf = 0;
+    a3Cd = 0; a3Ce = 0; a3Cf = 0;
+
+    b1Ad = 0; b1Ae = 0; b1Af = 0;
+    b1Bd = 0; b1Be = 0; b1Bf = 0;
+    b1Cd = 0; b1Ce = 0; b1Cf = 0;
+
+    b2Ad = 0; b2Ae = 0; b2Af = 0;
+    b2Bd = 0; b2Be = 0; b2Bf = 0;
+    b2Cd = 0; b2Ce = 0; b2Cf = 0;
+
+    b3Ad = 0; b3Ae = 0; b3Af = 0;
+    b3Bd = 0; b3Be = 0; b3Bf = 0;
+    b3Cd = 0; b3Ce = 0; b3Cf = 0;
+
+    c1Ad = 0; c1Ae = 0; c1Af = 0;
+    c1Bd = 0; c1Be = 0; c1Bf = 0;
+    c1Cd = 0; c1Ce = 0; c1Cf = 0;
+
+    c2Ad = 0; c2Ae = 0; c2Af = 0;
+    c2Bd = 0; c2Be = 0; c2Bf = 0;
+    c2Cd = 0; c2Ce = 0; c2Cf = 0;
+
+    c3Ad = 0; c3Ae = 0; c3Af = 0;
+    c3Bd = 0; c3Be = 0; c3Bf = 0;
+    c3Cd = 0; c3Ce = 0; c3Cf = 0;
+
+    size("a1Ad"); size("a1Ae"); size("a1Af");
+    size("a1Bd"); size("a1Be"); size("a1Bf");
+    size("a1Cd"); size("a1Ce"); size("a1Cf");
+
+    size("a2Ad"); size("a2Ae"); size("a2Af");
+    size("a2Bd"); size("a2Be"); size("a2Bf");
+    size("a2Cd"); size("a2Ce"); size("a2Cf");
+
+    size("a3Ad"); size("a3Ae"); size("a3Af");
+    size("a3Bd"); size("a3Be"); size("a3Bf");
+    size("a3Cd"); size("a3Ce"); size("a3Cf");
+
+    size("b1Ad"); size("b1Ae"); size("b1Af");
+    size("b1Bd"); size("b1Be"); size("b1Bf");
+    size("b1Cd"); size("b1Ce"); size("b1Cf");
+
+    size("b2Ad"); size("b2Ae"); size("b2Af");
+    size("b2Bd"); size("b2Be"); size("b2Bf");
+    size("b2Cd"); size("b2Ce"); size("b2Cf");
+
+    size("b3Ad"); size("b3Ae"); size("b3Af");
+    size("b3Bd"); size("b3Be"); size("b3Bf");
+    size("b3Cd"); size("b3Ce"); size("b3Cf");
+
+    size("c1Ad"); size("c1Ae"); size("c1Af");
+    size("c1Bd"); size("c1Be"); size("c1Bf");
+    size("c1Cd"); size("c1Ce"); size("c1Cf");
+
+    size("c2Ad"); size("c2Ae"); size("c2Af");
+    size("c2Bd"); size("c2Be"); size("c2Bf");
+    size("c2Cd"); size("c2Ce"); size("c2Cf");
+
+    size("c3Ad"); size("c3Ae"); size("c3Af");
+    size("c3Bd"); size("c3Be"); size("c3Bf");
+    size("c3Cd"); size("c3Ce"); size("c3Cf");
+}
+
+function yay() {
+    z++;
+    if (z === 1||z === -1) {
+        document.getElementById("z").innerHTML = z + " point";
+    } else {
+        document.getElementById("z").innerHTML = z + " points";
+    }
+}
+function oof() {
+    z--;
+    if (z === 1||z === -1) {
+        document.getElementById("z").innerHTML = z + " point";
+    } else {
+        document.getElementById("z").innerHTML = z + " points";
+    }
+}      
+function color() {
+    if (z < 0) {
+        document.getElementById("z").style.color="#d44"; 
+    }
+    else if (z > 0) {
+        document.getElementById("z").style.color="#4b4"; 
+    }
+    else {
+        document.getElementById("z").style.color="#44c"; 
+    }
+}
+function score(varr) {                      
+    document.getElementById(varr).style.transform = "scale(0.9,0.9)";
+    if (x % 3 === 0) {
+        if (a % 3 === 0 && a > 0) {
+            yay();
+        } else if (b % 3 === 0 && b > 0) {
+            yay();
+        } else if (c % 3 === 0 && c > 0) {
+            yay();
+        } else if (n1 % 3 === 0 && n1 > 0) {
+            yay();
+        } else if (n2 % 3 === 0 && n2 > 0) {
+            yay();
+        } else if (n3 % 3 === 0 && n3 > 0) {
+            yay();
+        } else if (A % 3 === 0 && A > 0) {
+            yay();
+        } else if (B % 3 === 0 && B > 0) {
+            yay();
+        } else if (C % 3 === 0 && C > 0) {
+            yay();
+        } else if (d % 3 === 0 && A > 0) {
+            yay();
+        } else if (e % 3 === 0 && B > 0) {
+            yay();
+        } else if (f % 3 === 0 && C > 0) {
+            yay();
+        } else {
+            oof();
+        }
+        zero();
+    }
+    color();
+}
+
+color();
+document.getElementById("a1Ad").onclick = function() {
+    if (a1Ad === 0) {
+        a++;
+        n1++;
+        A++;
+        d++;
+        a1Ad++;
+        x++;
+        score("a1Ad");
+    }
+}
+document.getElementById("a1Ae").onclick = function() {
+    if (a1Ae === 0) {
+        a++;
+        n1++;
+        A++;
+        e++;
+        a1Ae++;
+        x++;
+        score("a1Ae");
+    }
+}
+document.getElementById("a1Af").onclick = function() {
+    if (a1Af === 0) {
+        a++;
+        n1++;
+        A++;
+        f++
+        a1Af++;
+        x++;
+        score("a1Af");
+    }
+}
+document.getElementById("a1Bd").onclick = function() {
+    if (a1Bd === 0) {
+        a++;
+        n1++;
+        B++;
+        d++;
+        x++;
+        score("a1Bd");
+    }
+}
+document.getElementById("a1Be").onclick = function() {
+    if (a1Be === 0) {
+        a++;
+        n1++;
+        B++;
+        e++;
+        x++;
+        score("a1Be");
+    }
+}
+document.getElementById("a1Bf").onclick = function() {
+    if (a1Bf === 0) {
+        a++;
+        n1++;
+        B++;
+        f++;
+        x++;
+        score("a1Bf");
+    }
+}
+document.getElementById("a2C").onclick = function() {
+    if (a2C === 0) {
+        a++;
+        n2++;
+        C++;
+        x++;
+        score("a2C");
+    }
+}
+document.getElementById("a3A").onclick = function() {
+    if (a3A === 0) {
+        a++;
+        n3++;
+        A++;
+        x++;
+        score("a3A");
+    }
+}
+document.getElementById("b1B").onclick = function() {
+    if (b1B === 0) {
+        b++;
+        n1++;
+        B++;
+        x++;
+        score("b1B");
+    }
+}
+document.getElementById("b2C").onclick = function() {
+    if (b2C === 0) {
+        b++;
+        n2++;
+        C++;
+        x++;
+        score("b2C");
+    }
+}
+document.getElementById("b3A").onclick = function() {
+    if (b3A === 0) {
+        b++;
+        n3++;
+        A++;
+        x++;
+        score("b3A");
+    }
+}
+document.getElementById("b3B").onclick = function() {
+    if (b3B === 0) {
+        b++;
+        n3++;
+        B++;
+        x++;
+        score("b3B");
+    }
+}
+document.getElementById("c1C").onclick = function() {
+    if (a2C === 0) {
+        c++;
+        n1++;
+        C++;
+        x++;
+        score("c1C");
+    }
+}
+document.getElementById("c2A").onclick = function() {
+    if (c2A === 0) {
+        c++;
+        n2++;
+        A++;
+        x++;
+        score("c2A");
+    }
+}
+document.getElementById("c2B").onclick = function() {
+    if (c2B === 0) {
+        c++;
+        n2++;
+        B++;
+        x++;
+        score("c2B");
+    }
+}
+document.getElementById("c3C").onclick = function() {
+    if (c3C === 0) {
+        c++;
+        n3++;
+        C++;
+        x++;
+        score("c3C");
+    }
+}
+document.getElementById("reset").onclick = function reset() {
+    z = 0;
+    zero();
+    color();
+    document.getElementById("z").innerHTML = z + " points";
+}
