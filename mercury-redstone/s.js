573d747d71a1108ad96018355cb83e4abecc397c
FlyOrBoom <31298876+FlyOrBoom@users.noreply.github.com>
2019-01-16 16:47:08 -0800

    Add files via upload

--- mercury-redstone/s.js
+++ mercury-redstone/s.js
@@ -1,10 +1,10 @@
-var x = 0;
-var y = 0;
-var throttle = 0;
-var opa = ((100000-y)/100000);
-var quart = 0;
+var x = 0; //extranneous
+var y = 0; //height
+var throttle = 0; //self-explanatory
+var opa = ((100000-y)/100000); //opacity
+var quart = 0; //quarter of thing
 var screenHeight = window.innerHeight;
-var tick = 0;
+var tick = 0; //counter
 
 var t = [350000,1000]; //thrust of [stage]
 var f = [23800,800]; //fuel of [stage]
@@ -25,11 +25,11 @@ function draw() {
     if(y<0){
       v=0;
     }else{
-      v = v + ((throttle*t[s])/mTotal) - g;
+      v = v + (((throttle*t[s])/mTotal) - g)*0.064;
     }
   }else{
     f[s] = 0;
-    v = v-g;
+    v = v-g*8/125;
   }
   
   
@@ -81,6 +81,13 @@ function draw() {
       document.getElementById('shake1').style = 'transform:translate('+0.5*throttle+'px,0)';
       quart=0;
     }
+  }else{
+    document.getElementById('fa').style.opacity = 0;
+    document.getElementById('fb').style.opacity = 0;
+    document.getElementById('fc').style.opacity = 0;
+    document.getElementById('fd').style.opacity = 0;
+    document.getElementById('shake0').style = 'transform:translate(0,0)';
+    document.getElementById('shake1').style = 'transform:translate(0,0)';
   }
 
 //throttle
@@ -117,7 +124,7 @@ function draw() {
   if(y<0){
     y = 0;
   }else{
-    y = y + v*8/125;
+    y = y + v;
   }
 //overlay
   document.getElementById('tContainer').style.left = 0.02*screenHeight+'px';
@@ -126,8 +133,16 @@ function draw() {
   document.getElementById('tBar').style.left = 0.03*screenHeight+'px';
   document.getElementById('tBar').style.width = 0.04*screenHeight+'px';
   document.getElementById('tBar').style.height = 0.04*screenHeight+throttle*0.2275*screenHeight+'px';
-  document.getElementById('yContainer').innerHTML = 'altitude: '+Math.round(y)+' m';
-  document.getElementById('vContainer').innerHTML = 'velocity: '+Math.round(v)+' m/s';
+  if(y>0){
+    document.getElementById('yContainer').innerHTML = 'altitude: '+Math.round(y)+' m';
+  }else{
+    document.getElementById('yContainer').innerHTML = 'altitude: 0 m';
+  }
+  if(v>0||v<-1){
+    document.getElementById('vContainer').innerHTML = 'velocity: '+Math.round(v*125/8)+' m/s';
+  }else{
+    document.getElementById('vContainer').innerHTML = 'velocity: 0 m/s';
+  }
   document.getElementById('fContainer').innerHTML = 'fuel: '+Math.round(f[s]/238)+'%';
 }
 
